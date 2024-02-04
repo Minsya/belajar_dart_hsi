ca3cbe2db953d03ded737e8432b7858f14265463
Minsya <minsya.barakit@gmail.com>
2024-02-04 09:52:28 +0700

    update string file

--- bin/string.dart
+++ bin/string.dart
@@ -10,4 +10,19 @@ void main() {
 
   var text = 'this is \'dart\' \$cool';
   print(text);
+
+  var name1 = firstName + lastName;
+  var name2 = 'firstName + lastName' 'Eko';
+
+  print(name1);
+  print(name2);
+
+  var longString = '''
+Lorem Ipsum is simply dummy text of 
+the printing and typesetting industry. 
+Lorem Ipsum has been the industry's standard
+dummy text ever since the 1500s
+''';
+
+  print(longString);
 }
