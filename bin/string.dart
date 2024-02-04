b1d74a335f48c8facb2b86b77fc7a401269009f0
Minsya <minsya.barakit@gmail.com>
2024-02-04 09:41:19 +0700

    first commit

---
+++ bin/string.dart
@@ -0,0 +1,13 @@
+void main() {
+  String firstName = 'Amin';
+  String lastName = 'Syaharuddin';
+
+  print(firstName);
+  print(lastName);
+
+  var fullName = '$firstName ${lastName}';
+  print(fullName);
+
+  var text = 'this is \'dart\' \$cool';
+  print(text);
+}
---
+++ bin/variable.dart
@@ -0,0 +1,23 @@
+void main() {
+  var name = 'Amin Syaharuddin';
+  print(name);
+
+  String name2;
+  name2 = 'Amin Syaharuddin 2';
+  print(name2);
+
+  name2 = 'Tes name2';
+  print(name2);
+
+// Contoh Variable Final
+  var firstName = 'Amin';
+  final lastName = 'Syaharuddin';
+
+  print(firstName);
+  print(lastName);
+
+  firstName = 'Budi';
+  // lastName = 'Setiawan';
+  print(firstName);
+  print(lastName);
+}
